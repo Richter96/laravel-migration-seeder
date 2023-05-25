af62c475982411acf44ee2f7790010643bc5270f
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-25 19:21:51 +0200

    add card

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('home');
+        // dd(train::all());
+        $trains = train::all();
+        return view('home', compact('trains'));
     }
     public function contacts()
     {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,4 +6,25 @@
         <h1>I LOVE TRAINS</h1>
 
     </div>
+
+    <section>
+        <div class="container text-black">
+            <div class="row row-cols-5 g-4">
+                @forelse ($trains as $train)
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4>{{ $train->Azienda }}</h4>
+                            </div>
+                        </div>
+                    </div>
+
+                @empty
+                    <div class="col">
+                        <p>Torna piu tardin non ci sono treni</p>
+                    </div>
+                @endforelse
+            </div>
+        </div>
+    </section>
 @endsection
